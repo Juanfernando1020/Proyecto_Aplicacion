75c2874a4e86971de9f8b2053b986ec05ad58d19
Juanfernando1020 <juan.fnando.ar@gmail.com>
2023-03-28 22:38:20 -0500

    Version2

--- Aplicacion/Aplicacion.Android/Resources/Resource.designer.cs
+++ Aplicacion/Aplicacion.Android/Resources/Resource.designer.cs
@@ -16679,25 +16679,28 @@ namespace Aplicacion.Droid
 			public const int notify_panel_notification_icon_bg = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int pngegg__1_ = 2131165334;
+			public const int Password = 2131165334;
 			
 			// aapt resource value: 0x7F070097
-			public const int pngegg__2_ = 2131165335;
+			public const int pngegg__1_ = 2131165335;
 			
 			// aapt resource value: 0x7F070098
-			public const int preference_list_divider_material = 2131165336;
+			public const int pngegg__2_ = 2131165336;
 			
 			// aapt resource value: 0x7F070099
-			public const int test_custom_background = 2131165337;
+			public const int preference_list_divider_material = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
-			public const int tooltip_frame_dark = 2131165338;
+			public const int test_custom_background = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int tooltip_frame_light = 2131165339;
+			public const int tooltip_frame_dark = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int usuario = 2131165340;
+			public const int tooltip_frame_light = 2131165340;
+			
+			// aapt resource value: 0x7F07009D
+			public const int usuario = 2131165341;
 			
 			// aapt resource value: 0x7F070006
 			public const int _icon_awesome_user_edit = 2131165190;
