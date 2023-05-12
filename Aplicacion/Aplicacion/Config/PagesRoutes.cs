79941b048da9ea14194b26ed52073bdfc8d03ed5
Juanfernando1020 <juan.fnando.ar@gmail.com>
2023-05-11 23:37:19 -0500

    Add RoutePages

--- Aplicacion/Aplicacion/Config/PagesRoutes.cs
+++ Aplicacion/Aplicacion/Config/PagesRoutes.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Aplicacion.Config
 {
-    static class PagesRoutes
+    static class RoutePages
     {
         #region "User Pages"
         static string UserCreatePage => UserCreatePage;
