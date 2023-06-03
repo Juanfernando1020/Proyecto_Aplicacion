2aef3f45c4cbcdca5a0e91fd37637643c7870b81
Juanfernando1020 <juan.fnando.ar@gmail.com>
2023-06-02 22:43:13 -0500

    Implementation  the camera service and the necessary permissions in Android and Ios

--- Aplicacion/Aplicacion/Pages/Client/Details/ClientDetailsPage.xaml.cs
+++ Aplicacion/Aplicacion/Pages/Client/Details/ClientDetailsPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.CommonToolkit.PagesBase;
+﻿using Plugin.Media.Abstractions;
+using Xamarin.CommonToolkit.PagesBase;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,6 +12,40 @@ namespace Aplicacion.Pages.Client.Details
 		public ClientDetailsPage()
 		{
 			InitializeComponent();
+
+			TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += TapGestureRecognizer_Tapped;
+
+			TakePhoto.GestureRecognizers.Add(tapGestureRecognizer);
 		}
-	}
+
+        private async void TapGestureRecognizer_Tapped(object sender, System.EventArgs e)
+        {
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    PhotoSize = PhotoSize.Medium,
+                    SaveToAlbum = false,
+                    RotateImage = true,
+                    DefaultCamera = CameraDevice.Rear,
+                    AllowCropping = false,
+                    ModalPresentationStyle = MediaPickerModalPresentationStyle.OverFullScreen
+                });
+
+                if (photo != null)
+                {
+                    Photo.Source = ImageSource.FromStream(() =>
+                    {
+                        return photo.GetStream();
+                    });
+
+                    // Ocultar el icono
+                    IconLabel.IsVisible = false;
+                }
+            });
+        }
+
+
+    }
 }
