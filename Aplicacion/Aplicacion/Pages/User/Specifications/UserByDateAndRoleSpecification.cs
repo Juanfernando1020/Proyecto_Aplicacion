efeabb6a99f2c4634c6f85c22fb759f2e3fdf4bd
Juanfernando1020 <juan.fnando.ar@gmail.com>
2023-06-10 03:51:43 -0500

    Implement Specifications

---
+++ Aplicacion/Aplicacion/Pages/User/Specifications/UserByDateAndRoleSpecification.cs
@@ -0,0 +1,26 @@
+﻿using Aplicacion.Models;
+using Aplicacion.Pages.User.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Xamarin.CommonToolkit.Specifications;
+
+namespace Aplicacion.Pages.User.Specifications
+{
+    class UserByDateAndRoleSpecification : SpecificationBase<Users>
+    {
+        private readonly DateTime _date;
+        private readonly RolesEnum _role;
+
+        public UserByDateAndRoleSpecification(DateTime date, RolesEnum role)
+        {
+            _date = date;
+            _role = role;
+        }
+
+        public override Expression<Func<Users, bool>> ToExpression()
+           => user => user.NextPaymentDate.Date < DateTime.Now.Date && user.Role == (int)_role;
+
+    }
+}
---
+++ Aplicacion/Aplicacion/Pages/User/Specifications/UserByNotRoleSpecification.cs
@@ -0,0 +1,23 @@
+﻿using Aplicacion.Models;
+using Aplicacion.Pages.User.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Xamarin.CommonToolkit.Specifications;
+
+namespace Aplicacion.Pages.User.Specifications
+{
+    internal class UserByNotRoleSpecification : SpecificationBase<Users>
+    {
+        private readonly RolesEnum _role;
+
+        public  UserByNotRoleSpecification(RolesEnum role)
+        {
+            _role = role;
+        }
+
+        public override Expression<Func<Users, bool>> ToExpression()
+            => user => user.Role != (int)_role;
+    }
+}
